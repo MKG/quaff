39911aac825996c9d3dc3144807641796c6bfff7
Joel Falcou <joel.falcou@lri.fr>
2011-03-28 10:23:25 +0200

    Added missing include in action test

--- unit/action.cpp
+++ unit/action.cpp
@@ -7,8 +7,7 @@
  *                 See accompanying file LICENSE.txt or copy at
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
-#include <iostream>
-#include <typeinfo>
+#include <cstdio>
 #include <boost/function.hpp>
 #include <boost/utility/result_of.hpp>
 #include <quaff/sdk/meta/action.hpp>
