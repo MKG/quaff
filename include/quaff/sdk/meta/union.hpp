ee9113ebc07cfe7f61acec775c520d83d5590906
Joel Falcou <joel.falcou@lri.fr>
2011-04-19 12:05:59 -0500

    meta union adn concatenation now work for set and vector

--- include/quaff/sdk/meta/union.hpp
+++ include/quaff/sdk/meta/union.hpp
@@ -9,20 +9,45 @@
 #ifndef QUAFF_SDK_META_UNION_HPP_INCLUDED
 #define QUAFF_SDK_META_UNION_HPP_INCLUDED
 
-#include <boost/mpl/copy.hpp>
-#include <boost/mpl/back_inserter.hpp>
+#include <boost/fusion/include/join.hpp>
+#include <boost/fusion/include/as_set.hpp>
+#include <boost/fusion/include/as_vector.hpp>
+#include <boost/fusion/include/joint_view.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
-// Force build the union of two ExtendableSequence
+// Force build the union of two Sequence or two AssociativeSequence
 ////////////////////////////////////////////////////////////////////////////////
+namespace quaff { namespace result_of
+{
+  template<class S1,class S2>
+  struct  concatenate
+        : boost::fusion::result_of::
+          as_vector< boost::fusion::joint_view<S1,S2> >
+  {};
+
+  template<class S1,class S2>
+  struct  union_
+        : boost::fusion::result_of::
+          as_set< boost::fusion::joint_view<S1,S2> >
+  {};
+} }
 
 namespace quaff { namespace meta
 {
-  template<class T1,class T2>
-  struct  union_
-        : boost::mpl::copy<T2, boost::mpl::back_inserter<T1> >
+  template<class S1,class S2>
+  typename result_of::concatenate<S1 const,S2 const>::type
+  concatenate( S1 const& s1, S2 const& s2)
   {
-  };
+    return boost::fusion::as_vector( boost::fusion::join(s1,s2) );
+  }
+
+  template<class S1,class S2>
+  typename result_of::concatenate<S1 const,S2 const>::type
+  union_( S1 const& s1, S2 const& s2)
+  {
+    return boost::fusion::as_set( boost::fusion::join(s1,s2) );
+  }
+
 } }
 
 #endif
