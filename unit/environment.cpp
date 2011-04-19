50f1ce9727a7382b2c491f294c57b2b560aee86d
root <root@is006084.(none)>
2011-04-19 16:38:49 +0200

    begin of a testing function in environment.hpp
    the testing function try to print some of the PIDs from a given network.
    I can't take the process.

--- unit/environment.cpp
+++ unit/environment.cpp
@@ -12,6 +12,7 @@
 #include <quaff/core/backend.hpp>
 #include <quaff/core/skeleton/seq.hpp>
 #include <quaff/core/skeleton/source.hpp>
+#include <quaff/sdk/meta/transform_if.hpp>
  
 template<class X> void run(X const& x)
 {
@@ -36,17 +37,36 @@ int g()
   return i;
 }
 
+
+
+template<class Network> void test(Network const& n) {
+
+//tentative pour récuperer le process
+//typedef typename quaff::model::process process;
+
+quaff::meta::transform_if<Network const>(n
+                          ,(std::cout << "pid :" 
+                                      << Network::process::pid_type::value)
+                           ,((Network::process::pid_type::value %2)==0) );
+}
+
 int main()
 {
   debug( quaff::source(g) & (quaff::source(g) & quaff::source(g) ), std::cout );
+quaff::semantic::convert<quaff::tag::process_network_>  converter;
+quaff::model::empty_environment                         env;
+quaff::backend::debug_ target;
 
+/*test( 
+    converter(( quaff::source(g) & (quaff::source(g) & quaff::source(g) )),env,target).network()
+     );*/
 
-  std::ofstream out("out.txt");
+/*  std::ofstream out("out.txt");
   debug(  ( quaff::source(g) & quaff::source(g) ) & quaff::source(g)
       & ( quaff::source(g) & quaff::source(g) ) & quaff::source(g)
       & (( quaff::source(g) & quaff::source(g) ) & quaff::source(g))
       , out
-      );
+      );*/
 
 /*
   debug(  ( quaff::source(g) & quaff::source(g) ) & quaff::source(g)
