07ab9213f05987822079f0bf5c4e67c9087fad7a
Joel Falcou <joel.falcou@lri.fr>
2011-04-20 21:54:26 -0500

    Small pipeline test and example

--- unit/environment.cpp
+++ unit/environment.cpp
@@ -8,22 +8,14 @@
  *                     http://www.boost.org/LICENSE_1_0.txt
  ******************************************************************************/
 #include <iostream>
-#include <fstream>
 #include <quaff/core/backend.hpp>
 #include <quaff/core/skeleton/seq.hpp>
 #include <quaff/core/skeleton/source.hpp>
-#include <quaff/sdk/meta/transform_if.hpp>
-#include <boost/fusion/include/io.hpp>
-#include <boost/fusion/include/join.hpp>
-#include <boost/fusion/include/set.hpp>
+#include <quaff/core/skeleton/sink.hpp>
 
-#include <boost/type_traits/is_integral.hpp>
-#include <boost/bind.hpp>
- 
 int g()
 {
   static int i(0);
-  std::cout << " - i = " << i << "\n";
 
   if(i < 3) i++;
   else
@@ -35,9 +27,19 @@ int g()
   return i;
 }
 
+int x2(int i)
+{
+  return 2 * i;
+}
+
+void h(int i)
+{
+  std::cout << "h say : " << i << "\n";
+}
+
 int main()
 {
-  debug( quaff::source(g) & (quaff::source(g) & quaff::source(g) ), std::cout );
-  run( quaff::source(g) & (quaff::source(g)& quaff::source(g)) & quaff::source(g) );
+  debug( quaff::source(g) | quaff::seq(x2) | quaff::sink(h), std::cout );
+  run( quaff::source(g) | quaff::seq(x2) | quaff::sink(h) );
 }
 
